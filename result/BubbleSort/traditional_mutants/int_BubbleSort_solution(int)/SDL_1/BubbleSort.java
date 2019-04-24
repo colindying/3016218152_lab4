d6a082e45b9a2520ecde56b403282328c2c531f5
小小@DESKTOP-0IVG9QQ <570627847@qq.com>
2019-04-24 14:19:03 +0800

    lab4

---
+++ result/BubbleSort/traditional_mutants/int_BubbleSort_solution(int)/SDL_1/BubbleSort.java
@@ -0,0 +1,23 @@
+// This is a mutant program.
+// Author : ysma
+
+import java.util.Arrays;
+
+
+public class BubbleSort
+{
+
+    public static  int[] BubbleSort_solution( int[] arr )
+    {
+        int temp;
+        return arr;
+    }
+
+    public static  void main( java.lang.String[] args )
+    {
+        int[] arr = new int[]{ 1, 6, 2, 2, 5 };
+        BubbleSort.BubbleSort_solution( arr );
+        System.out.println( Arrays.toString( arr ) );
+    }
+
+}
---
+++ result/BubbleSort/traditional_mutants/int_BubbleSort_solution(int)/SDL_2/BubbleSort.java
@@ -0,0 +1,25 @@
+// This is a mutant program.
+// Author : ysma
+
+import java.util.Arrays;
+
+
+public class BubbleSort
+{
+
+    public static  int[] BubbleSort_solution( int[] arr )
+    {
+        int temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+        }
+        return arr;
+    }
+
+    public static  void main( java.lang.String[] args )
+    {
+        int[] arr = new int[]{ 1, 6, 2, 2, 5 };
+        BubbleSort.BubbleSort_solution( arr );
+        System.out.println( Arrays.toString( arr ) );
+    }
+
+}
